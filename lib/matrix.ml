03af9530da4e860613561ee51009c5f5e348fa30
Simon McDonnell <simonmcdonnell@simons-mac-mini.home>
2023-07-18 15:03:03 -0400

    refactor matrix lib

--- lib/matrix.ml
+++ lib/matrix.ml
@@ -5,15 +5,18 @@ type t = { data : float array array; shape : int * int } [@@deriving show]
 type op = Add | Sub | Mul
 
 
+(* --------------------------------------- INITIALIZATION --------------------------------------- *)
+
+
 let shape data = (Array.length data, Array.length data.(0))
 
 let from_array data = { data; shape = shape data }
 
 let ones n_rows n_cols = 
-  Array.init ~f:(fun _ -> Array.init ~f:(fun _ -> 1.) n_cols) n_rows |> from_array
+  Array.init n_rows ~f:(fun _ -> Array.init n_cols ~f:(fun _ -> 1.))  |> from_array
 
 let zeros n_rows n_cols = 
-  Array.init ~f:(fun _ -> Array.init ~f:(fun _ -> 0.) n_cols) n_rows |> from_array
+  Array.init n_rows ~f:(fun _ -> Array.init n_cols ~f:(fun _ -> 0.))  |> from_array
 
 let he_init n_rows n_cols = 
   (* use 'He' initialization technique. *)
@@ -31,40 +34,34 @@ let he_init n_rows n_cols =
   in
   { data; shape = (n_rows, n_cols)}
 
-let apply_elem ~f data = Array.map ~f:(fun row -> Array.map ~f row) data
+
+(* ----------------------------------------- UNARY OPS ----------------------------------------- *)
+
+
+let apply_elem data ~f = Array.map data ~f:(fun row -> Array.map row ~f)
 
 let pow mat ~n = 
-  let result = apply_elem ~f:(fun x -> x **. n) mat.data in
+  let result = apply_elem mat.data ~f:(fun x -> x **. n) in
   { data = result; shape = mat.shape }
 
 let mul mat ~const = 
-  let result = apply_elem ~f:(fun x -> x *. const) mat.data in
+  let result = apply_elem mat.data ~f:(fun x -> x *. const) in
   { data = result; shape = mat.shape }
 
 let sigmoid mat = 
   let sig_fn x = 1. /. (1. +. Float.exp (Float.neg x)) in
-  let result = apply_elem ~f:(fun x -> sig_fn x) mat.data in
+  let result = apply_elem mat.data ~f:(fun x -> sig_fn x) in
   { data = result; shape = mat.shape }
 
-let apply ~f mat = 
-  let data = apply_elem ~f mat.data in
+let apply mat ~f = 
+  let data = apply_elem mat.data ~f in
   { data; shape = mat.shape }
 
 let replace mat ~cond ~new_val = 
-  let result = Array.map ~f:(fun row -> 
-    Array.map ~f:(fun i -> if cond i then new_val else i) row) mat.data
+  let result = Array.map mat.data ~f:(fun row -> 
+    Array.map row ~f:(fun i -> if cond i then new_val else i))
   in
   { data = result; shape = mat.shape }
-
-let matrix_op ~op d1 d2 = 
-  (* add or subtract matrices *)
-  let op_fn ~op d1 d2 = 
-    match op with
-    | Add -> Array.map2_exn ~f:(fun x y -> x +. y) d1 d2
-    | Sub -> Array.map2_exn ~f:(fun x y -> x -. y) d1 d2
-    | Mul -> Array.map2_exn ~f:(fun x y -> x *. y) d1 d2
-  in
-  Array.map2_exn ~f:(op_fn ~op) d1 d2
 
 let transp data =
   let num_rows, num_cols = shape data in
@@ -74,16 +71,32 @@ let transp data =
 let transpose mat = 
   { data = transp mat.data; shape = (snd mat.shape, fst mat.shape) }
 
-let repeat_elem ~n elem = Array.init n ~f:(fun _ -> elem)
+
+ (* ---------------------------------------- BINARY OPS ---------------------------------------- *)
+
+
+let matrix_op ~op d1 d2 = 
+  (* add or subtract matrices *)
+  let op_fn ~op d1 d2 = 
+    match op with
+    | Add -> Array.map2_exn d1 d2 ~f:(fun x y -> x +. y)
+    | Sub -> Array.map2_exn d1 d2 ~f:(fun x y -> x -. y)
+    | Mul -> Array.map2_exn d1 d2 ~f:(fun x y -> x *. y)
+  in
+  Array.map2_exn d1 d2 ~f:(op_fn ~op)
+
+let broadcast_reshape mat ~n_rows = 
+  let repeat_elem ~n elem = Array.init n ~f:(fun _ -> elem) in
+  Array.map mat.data.(0) ~f:(repeat_elem ~n:n_rows) |> transp
 
 let sum m1 m2 =
   (* if not same shape, check for array broadcast and alter shape to make addition work *)
   if fst m1.shape = 1 then
-    let m1_reshaped = Array.map ~f:(repeat_elem ~n:(fst m2.shape)) m1.data.(0) |> transp in
+    let m1_reshaped = broadcast_reshape m1 ~n_rows:(fst m2.shape) in
     let result = matrix_op ~op:Add m1_reshaped m2.data in
     { data = result; shape = m2.shape }
   else if fst m2.shape = 1 then
-    let m2_reshaped = Array.map ~f:(repeat_elem ~n:(fst m1.shape)) m2.data.(0) |> transp in
+    let m2_reshaped = broadcast_reshape m2 ~n_rows:(fst m1.shape) in
     let result = matrix_op ~op:Add m1.data m2_reshaped in
     { data = result; shape = m1.shape }
   else 
@@ -93,11 +106,11 @@ let sum m1 m2 =
 let sub m1 m2 =
   (* if not same shape, check for array broadcast and alter shape to make subtraction work *)
   if fst m1.shape = 1 then
-    let m1_reshaped = Array.map ~f:(repeat_elem ~n:(fst m2.shape)) m1.data.(0) |> transp in
+    let m1_reshaped = broadcast_reshape m1 ~n_rows:(fst m2.shape) in
     let result = matrix_op ~op:Sub m1_reshaped m2.data in
     { data = result; shape = m2.shape }
   else if fst m2.shape = 1 then
-    let m2_reshaped = Array.map ~f:(repeat_elem ~n:(fst m1.shape)) m2.data.(0) |> transp in
+    let m2_reshaped = broadcast_reshape m2 ~n_rows:(fst m1.shape) in
     let result = matrix_op ~op:Sub m1.data m2_reshaped in
     { data = result; shape = m1.shape }
   else 
--- lib/matrix.mli
+++ lib/matrix.mli
@@ -1,18 +1,19 @@
-type t = { data : float array array; shape : int * int }
+type t = { data : float array array; shape : int * int; }
 
+val pp : Format.formatter -> t -> unit
 val show : t -> string
 val from_array : float array array -> t
 val ones : int -> int -> t
 val zeros : int -> int -> t
+val he_init : int -> int -> t
+val pow : t -> n:float -> t
+val mul : t -> const:float -> t
+val sigmoid : t -> t
+val apply : t -> f:(float -> float) -> t
+val replace : t -> cond:(float -> bool) -> new_val:float -> t
+val transpose : t -> t
 val sum : t -> t -> t
 val sub : t -> t -> t
 val matmul : t -> t -> t
-val pow : t -> n:float -> t
-val mul : t -> const:float -> t
-val apply : f:(float -> float) -> t -> t
-val transpose : t -> t
-val replace : t -> cond:(float -> bool) -> new_val:float -> t
 val multiply : t -> t -> t
 val divide : t -> t -> t
-val sigmoid : t -> t
-val he_init : int -> int -> t
